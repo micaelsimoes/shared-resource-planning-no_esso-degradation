c3a447ff0ae5e6143a4cb81336b2097dcda38d6e
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-04-08 15:43:25 +0100

    ess_relax_secondary_reserve field removed.

--- shared_energy_storage_parameters.py
+++ shared_energy_storage_parameters.py
@@ -19,7 +19,6 @@ class SharedEnergyStorageParameters:
         self.ess_relax_capacity_available = False       # Capacity available (relax/use slack variables or not)
         self.ess_relax_capacity_degradation = False     # Capacity degradation (relax/use slack variables or not)
         self.ess_relax_capacity_relative = False        # Relative capacity (relax/use slack variables or not)
-        self.ess_relax_secondary_reserve = False        # Secondary reserve (relax/use slack variables or not)
         self.ess_interface_relax = False                # Expected interface values (relax/use slack variables or not)
         self.slacks_used = False
         self.plot_results = False                       # Plot results
@@ -47,7 +46,6 @@ def _read_parameters_from_file(planning_parameters, filename):
     planning_parameters.ess_relax_capacity_available = bool(params_data['ess_relax_capacity_available'])
     planning_parameters.ess_relax_capacity_degradation = bool(params_data['ess_relax_capacity_degradation'])
     planning_parameters.ess_relax_capacity_relative = bool(params_data['ess_relax_capacity_relative'])
-    planning_parameters.ess_relax_secondary_reserve = bool(params_data['ess_relax_secondary_reserve'])
     planning_parameters.ess_interface_relax = bool(params_data['ess_interface_relax'])
     planning_parameters.print_results_to_file = bool(params_data['print_results_to_file'])
     planning_parameters.solver_params.read_solver_parameters(params_data['solver'])
@@ -55,6 +53,5 @@ def _read_parameters_from_file(planning_parameters, filename):
     if planning_parameters.ess_relax_comp or planning_parameters.ess_relax_apparent_power or\
             planning_parameters.ess_relax_soc or planning_parameters.ess_relax_day_balance or\
             planning_parameters.ess_relax_installed_capacity or planning_parameters.ess_relax_capacity_available or\
-            planning_parameters.ess_relax_capacity_degradation or planning_parameters.ess_relax_secondary_reserve or\
-            planning_parameters.ess_interface_relax:
+            planning_parameters.ess_relax_capacity_degradation or planning_parameters.ess_interface_relax:
         planning_parameters.slacks_used = True
