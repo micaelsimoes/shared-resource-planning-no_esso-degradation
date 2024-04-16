6edbbbcb36dbb198b07f95f7e736a64e1ca1a4e5
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-04-16 16:57:23 +0100

    Debug.

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -970,8 +970,8 @@ def update_distribution_coordination_models_and_solve(distribution_networks, mod
 
                 # Update POWER FLOW variables at connection point
                 for p in model[year][day].periods:
-                    model[year][day].dual_pf_p_req[p].fix(-dual_pf['current'][node_id][year][day]['p'][p] / s_base)
-                    model[year][day].dual_pf_q_req[p].fix(-dual_pf['current'][node_id][year][day]['q'][p] / s_base)
+                    model[year][day].dual_pf_p_req[p].fix(dual_pf['current'][node_id][year][day]['p'][p] / s_base)
+                    model[year][day].dual_pf_q_req[p].fix(dual_pf['current'][node_id][year][day]['q'][p] / s_base)
                     model[year][day].p_pf_req[p].fix(pf_req['current'][node_id][year][day]['p'][p] / s_base)
                     model[year][day].q_pf_req[p].fix(pf_req['current'][node_id][year][day]['q'][p] / s_base)
                     #model[year][day].dual_pf_p_prev[p].fix(dual_pf['prev'][node_id][year][day]['p'][p] / s_base)
