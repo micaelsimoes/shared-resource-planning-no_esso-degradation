bbce96ffbeb761c570f0c1b2500858cd5c7db769
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-05-03 15:21:24 +0100

    Test

--- main.py
+++ main.py
@@ -66,6 +66,8 @@ def shared_resources_planning(working_directory, specification_filename):
     #planning_problem.run_without_coordination(print_results=True)
 
     transmission_network = planning_problem.transmission_network
+    candidate_solution = planning_problem.get_initial_candidate_solution()
+    transmission_network.update_data_with_candidate_solution(candidate_solution)
     tn_model = transmission_network.build_model()
     results = transmission_network.optimize(tn_model)
     processed_results = transmission_network.process_results(tn_model, results)
