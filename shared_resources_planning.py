a662a0e4875320d4f5c40834eb7993a0139b0263
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-05-07 08:56:33 +0100

    Test

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -380,6 +380,7 @@ def create_shared_energy_storage_model(shared_ess_data, sess_vars, candidate_sol
                     esso_model.es_pnet[e, y, d, p].fix(preq)
                     esso_model.es_pnet[e, y, d, p].fix(qreq)
     results = shared_ess_data.optimize(esso_model)
+    shared_ess_data.write_optimization_results_to_excel(esso_model)
 
     for e in esso_model.energy_storages:
         node_id = shared_ess_data.active_distribution_network_nodes[e]
