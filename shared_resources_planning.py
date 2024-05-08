fd75eeee04c8b2d72bdcc6f63a1cb7368e61ea5d
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-05-08 17:33:20 +0100

    create_shared_energy_storage_model(). S removed

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -377,9 +377,12 @@ def create_shared_energy_storage_model(shared_ess_data, sess_vars, candidate_sol
             for d in esso_model.days:
                 day = days[d]
                 for p in esso_model.periods:
-                    shared_ess_s = pe.value(esso_model.es_snet[e, y, d, p])
-                    sess_vars['esso']['current'][node_id][year][day]['s'][p] = shared_ess_s
-                    sess_vars['esso']['prev'][node_id][year][day]['s'][p] = shared_ess_s
+                    shared_ess_p = pe.value(esso_model.es_pnet[e, y, d, p])
+                    shared_ess_q = pe.value(esso_model.es_qnet[e, y, d, p])
+                    sess_vars['esso']['current'][node_id][year][day]['p'][p] = shared_ess_p
+                    sess_vars['esso']['current'][node_id][year][day]['q'][p] = shared_ess_q
+                    sess_vars['esso']['prev'][node_id][year][day]['p'][p] = shared_ess_p
+                    sess_vars['esso']['prev'][node_id][year][day]['q'][p] = shared_ess_q
 
     return esso_model, results
 
