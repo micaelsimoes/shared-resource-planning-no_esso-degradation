d38bbf96e00d0b3a4cfbd8955e23ccd33ebe7527
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-05-15 17:56:02 +0100

    Update

--- main.py
+++ main.py
@@ -61,11 +61,12 @@ def shared_resources_planning(working_directory, specification_filename):
     planning_problem.read_planning_problem()
     planning_problem.plot_diagram()
 
-    #candidate_solution = planning_problem.get_initial_candidate_solution()
-    #planning_problem.run_operational_planning(candidate_solution=candidate_solution, print_results=True, debug_flag=False)
+    candidate_solution = planning_problem.get_initial_candidate_solution()
+    planning_problem.run_operational_planning(candidate_solution=candidate_solution, print_results=True, debug_flag=False)
     #planning_problem.run_without_coordination(print_results=True)
     #planning_problem.run_planning_problem()
 
+    '''
     transmission_network = planning_problem.transmission_network
     candidate_solution = planning_problem.get_initial_candidate_solution()
     tn_model = transmission_network.build_model()
@@ -73,7 +74,9 @@ def shared_resources_planning(working_directory, specification_filename):
     results = transmission_network.optimize(tn_model)
     processed_results = transmission_network.process_results(tn_model, results)
     transmission_network.write_optimization_results_to_excel(processed_results)
+    '''
 
+    '''
     distribution_networks = planning_problem.distribution_networks
     for node_id in distribution_networks:
         distribution_network = distribution_networks[node_id]
@@ -82,6 +85,7 @@ def shared_resources_planning(working_directory, specification_filename):
         results = distribution_network.optimize(dn_model)
         processed_results = distribution_network.process_results(dn_model, results)
         distribution_network.write_optimization_results_to_excel(processed_results)
+    '''
 
     '''
     candidate_solution = planning_problem.get_initial_candidate_solution()
