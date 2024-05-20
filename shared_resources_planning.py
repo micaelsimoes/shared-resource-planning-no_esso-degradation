6a26fd481ba30edafb6cbcefc495efff6ba62c69
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-05-20 15:53:29 +0100

    Update

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -284,6 +284,25 @@ def _run_operational_planning(planning_problem, candidate_solution, debug_flag=F
             break
 
         # --------------------------------------------------------------------------------------------------------------
+        # 4. Solve ESSO problem
+        results['esso'] = update_shared_energy_storages_coordination_model_and_solve(planning_problem, esso_model,
+                                                                                     consensus_vars['ess'], dual_vars['ess']['esso'],
+                                                                                     admm_parameters, from_warm_start=from_warm_start)
+
+        # 4.1 Update ADMM CONSENSUS variables
+        planning_problem.update_admm_consensus_variables(tso_model, dso_models, esso_model,
+                                                         consensus_vars, dual_vars,
+                                                         admm_parameters)
+
+        # 4.2 Update primal evolution
+        primal_evolution.append(planning_problem.get_primal_value(tso_model, dso_models, esso_model))
+
+        # 4.3 STOPPING CRITERIA evaluation
+        convergence = check_admm_convergence(planning_problem, consensus_vars, admm_parameters)
+        if convergence:
+            break
+
+        # --------------------------------------------------------------------------------------------------------------
         # 3. Solve DSOs problems
         results['dso'] = update_distribution_coordination_models_and_solve(distribution_networks, dso_models,
                                                                            consensus_vars['interface']['v_sqr'], dual_vars['v_sqr']['dso'],
