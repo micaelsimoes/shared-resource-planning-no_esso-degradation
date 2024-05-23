00e0c52b9102761246ee8a852de28e5e4d795778
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-05-23 17:59:10 +0100

    Penalty ESS.

--- network.py
+++ network.py
@@ -991,17 +991,17 @@ def _build_model(network, params):
                     for p in model.periods:
                         if params.slacks.ess.complementarity:
                             slack_comp = model.slack_es_comp[e, s_m, s_o, p]
-                            obj += PENALTY_SLACK * network.baseMVA * slack_comp
+                            obj += PENALTY_ESS * network.baseMVA * slack_comp
                         if params.slacks.ess.charging:
                             slack_sch = model.slack_es_sch_up[e, s_m, s_o, p] + model.slack_es_sch_down[e, s_m, s_o, p]
                             slack_sdch = model.slack_es_sdch_up[e, s_m, s_o, p] + model.slack_es_sdch_down[e, s_m, s_o, p]
-                            obj += PENALTY_SLACK * network.baseMVA * (slack_sch + slack_sdch)
+                            obj += PENALTY_ESS * network.baseMVA * (slack_sch + slack_sdch)
                         if params.slacks.ess.soc:
                             slack_soc = model.slack_es_soc_up[e, s_m, s_o, p] + model.slack_es_soc_down[e, s_m, s_o, p]
-                            obj += PENALTY_SLACK * network.baseMVA * slack_soc
+                            obj += PENALTY_ESS * network.baseMVA * slack_soc
                     if params.slacks.ess.day_balance:
                         slack_soc_final = model.slack_es_soc_final_up[e, s_m, s_o] + model.slack_es_soc_final_down[e, s_m, s_o]
-                        obj += PENALTY_SLACK * network.baseMVA * slack_soc_final
+                        obj += PENALTY_ESS * network.baseMVA * slack_soc_final
 
             # Shared ESS slacks
             for e in model.shared_energy_storages:
