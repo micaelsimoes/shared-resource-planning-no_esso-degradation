422a764d540808431c9fb35f6eaf65b5bd0588d6
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-05-23 16:04:33 +0100

    Debug.

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ def shared_resources_planning(working_directory, specification_filename):
     #planning_problem.run_without_coordination(print_results=True)
     #planning_problem.run_planning_problem()
 
+    '''
     transmission_network = planning_problem.transmission_network
     candidate_solution = planning_problem.get_initial_candidate_solution()
     tn_model = transmission_network.build_model()
@@ -73,18 +74,20 @@ def shared_resources_planning(working_directory, specification_filename):
     results = transmission_network.optimize(tn_model)
     processed_results = transmission_network.process_results(tn_model, results)
     transmission_network.write_optimization_results_to_excel(processed_results)
-
     '''
-    candidate_solution = planning_problem.get_initial_candidate_solution()
+
+    #candidate_solution = planning_problem.get_initial_candidate_solution()
     distribution_networks = planning_problem.distribution_networks
     for node_id in distribution_networks:
         distribution_network = distribution_networks[node_id]
+        for year in planning_problem.years:
+            for day in planning_problem.days:
+                distribution_network[year][day].shared_energy_storages = list()
         dn_model = distribution_network.build_model()
-        distribution_network.update_model_with_candidate_solution(dn_model, candidate_solution['total_capacity'])
+        #distribution_network.update_model_with_candidate_solution(dn_model, candidate_solution['total_capacity'])
         results = distribution_network.optimize(dn_model)
         processed_results = distribution_network.process_results(dn_model, results)
         distribution_network.write_optimization_results_to_excel(processed_results)
-    '''
 
     '''
     candidate_solution = planning_problem.get_initial_candidate_solution()
