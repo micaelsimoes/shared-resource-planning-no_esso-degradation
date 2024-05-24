522eb3c1569bb9acd8c1379effec2af220d29a82
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-05-24 11:29:12 +0100

    Test

--- network.py
+++ network.py
@@ -964,7 +964,7 @@ def _build_model(network, params):
     for e in model.shared_energy_storages:
         slack_s = model.shared_es_s_slack_up[e] + model.shared_es_s_slack_down[e]
         slack_e = model.shared_es_e_slack_up[e] + model.shared_es_e_slack_down[e]
-        obj += PENALTY_SHARED_ESS * 24 * 3 * network.baseMVA * (slack_s + slack_e)
+        obj += PENALTY_SHARED_ESS * network.num_instants * len(network.prob_market_scenarios) * len(network.prob_operation_scenarios)  * network.baseMVA * (slack_s + slack_e)
 
     # Operation slacks
     for s_m in model.scenarios_market:
