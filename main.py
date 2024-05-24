1233926e5a96f4d69a06a0b1d7e2d1091982dda0
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-05-24 11:52:33 +0100

    Test

--- main.py
+++ main.py
@@ -73,16 +73,15 @@ def shared_resources_planning(working_directory, specification_filename):
     results = transmission_network.optimize(tn_model)
     processed_results = transmission_network.process_results(tn_model, results)
     transmission_network.write_optimization_results_to_excel(processed_results)
+    sensitivities = transmission_network.get_sensitivities(tn_model)
+    print(sensitivities)
 
-    #candidate_solution = planning_problem.get_initial_candidate_solution()
+    candidate_solution = planning_problem.get_initial_candidate_solution()
     distribution_networks = planning_problem.distribution_networks
     for node_id in distribution_networks:
         distribution_network = distribution_networks[node_id]
-        for year in distribution_network.years:
-            for day in distribution_network.days:
-                distribution_network.network[year][day].shared_energy_storages = list()
         dn_model = distribution_network.build_model()
-        #distribution_network.update_model_with_candidate_solution(dn_model, candidate_solution['total_capacity'])
+        distribution_network.update_model_with_candidate_solution(dn_model, candidate_solution['total_capacity'])
         results = distribution_network.optimize(dn_model)
         processed_results = distribution_network.process_results(dn_model, results)
         distribution_network.write_optimization_results_to_excel(processed_results)
