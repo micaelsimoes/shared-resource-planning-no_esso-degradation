f4c998c7e34db5352bd66161d95721962304bc57
micaelsimoes <micaelsimoes@gmail.com>
2024-05-28 09:02:14 +0100

    Debug

--- main.py
+++ main.py
@@ -74,8 +74,8 @@ def shared_resources_planning(working_directory, specification_filename):
     processed_results = transmission_network.process_results(tn_model, results)
     transmission_network.write_optimization_results_to_excel(processed_results)
     sensitivities = transmission_network.get_sensitivities(tn_model)
+    print(sensitivities)
 
-    '''
     candidate_solution = planning_problem.get_initial_candidate_solution()
     distribution_networks = planning_problem.distribution_networks
     for node_id in distribution_networks:
@@ -85,7 +85,6 @@ def shared_resources_planning(working_directory, specification_filename):
         results = distribution_network.optimize(dn_model)
         processed_results = distribution_network.process_results(dn_model, results)
         distribution_network.write_optimization_results_to_excel(processed_results)
-    '''
 
     '''
     candidate_solution = planning_problem.get_initial_candidate_solution()
