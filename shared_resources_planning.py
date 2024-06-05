8b24a8bf2edc553c0a6d54c83acda03c9af6aae5
micaelsimoes <micaelsimoes@gmail.com>
2024-06-05 17:47:48 +0100

    Update

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -1994,7 +1994,7 @@ def _write_operational_planning_main_info_per_operator(network, sheet, operator_
     col_idx += 1
     for year in results:
         for day in results[year]:
-            sheet.cell(row=line_idx, column=col_idx).value = results[year][day]['load_curt']['p']
+            sheet.cell(row=line_idx, column=col_idx).value = results[year][day]['total_load']['p']
             sheet.cell(row=line_idx, column=col_idx).number_format = decimal_style
             col_idx += 1
 
@@ -2008,7 +2008,7 @@ def _write_operational_planning_main_info_per_operator(network, sheet, operator_
     col_idx += 1
     for year in results:
         for day in results[year]:
-            sheet.cell(row=line_idx, column=col_idx).value = results[year][day]['load_curt']['q']
+            sheet.cell(row=line_idx, column=col_idx).value = results[year][day]['total_load']['q']
             sheet.cell(row=line_idx, column=col_idx).number_format = decimal_style
             col_idx += 1
 
