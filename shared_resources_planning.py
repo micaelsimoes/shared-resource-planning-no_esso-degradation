f4f70da26aa63367b56542279e23a8262066eb4e
micaelsimoes <micaelsimoes@gmail.com>
2024-06-21 14:07:00 +0100

    Update

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -4850,10 +4850,10 @@ def _get_initial_candidate_solution(planning_problem):
             candidate_solution['total_capacity'][node_id][year] = dict()
             candidate_solution['total_capacity'][node_id][year]['s'] = 0.00
             candidate_solution['total_capacity'][node_id][year]['e'] = 0.00
-            if year == 2020:
+            if year == 2020 or year == 2035:
                 candidate_solution['investment'][node_id][year]['s'] = 1.00
                 candidate_solution['investment'][node_id][year]['e'] = 1.00
-            if year < 2030:
+            if (2020 <= year < 2030) or (2035 <= year < 2045):
                 candidate_solution['total_capacity'][node_id][year]['s'] = 1.00
                 candidate_solution['total_capacity'][node_id][year]['e'] = 1.00
     return candidate_solution
