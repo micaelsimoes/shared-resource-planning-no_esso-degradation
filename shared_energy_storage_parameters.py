af76fd7b5a521c9feb49bbbafe369e3e7e9f1027
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-06-21 15:08:36 +0100

    Update. "Slacks" structure added.

--- shared_energy_storage_parameters.py
+++ shared_energy_storage_parameters.py
@@ -12,14 +12,31 @@ class SharedEnergyStorageParameters:
         self.max_capacity = 2.50                        # Max energy capacity (related to space constraints)
         self.min_pe_factor = 0.10                       # Minimum S/E factor (related to the ESS technology)
         self.max_pe_factor = 4.00                       # Maximum S/E factor (related to the ESS technology)
-        self.slacks = False                             # Relax/use slack variables
+        self.relax_equalities = False                   # Relax equality constraints
+        self.slacks = Slacks()                          # Relax/use slack variables
         self.plot_results = False                       # Plot results
-        self.print_results_to_file = False              # Write results to file
         self.verbose = False                            # Verbose -- Bool
         self.solver_params = SolverParameters()         # Solver Parameters
+        self.print_results_to_file = False              # Write results to file
 
     def read_parameters_from_file(self, filename):
         _read_parameters_from_file(self, filename)
+
+
+# ======================================================================================================================
+#   Slack Classes
+# ======================================================================================================================
+class Slacks:
+
+    def __init__(self):
+        self.rated_power = False
+        self.rated_capacity = False
+
+    def read_slacks_parameters(self, slacks_data):
+        if 'rated_power' in slacks_data:
+            self.rated_power = slacks_data['rated_power']
+        if 'rated_capacity' in slacks_data:
+            self.rated_power = slacks_data['rated_capacity']
 
 
 def _read_parameters_from_file(planning_parameters, filename):
@@ -30,6 +47,9 @@ def _read_parameters_from_file(planning_parameters, filename):
     planning_parameters.max_capacity = float(params_data['max_capacity'])
     planning_parameters.min_pe_factor = float(params_data['min_pe_factor'])
     planning_parameters.max_pe_factor = float(params_data['max_pe_factor'])
-    planning_parameters.slacks = bool(params_data['slacks'])
+    planning_parameters.relax_equalities = bool(params_data['relax_equalities'])
+    if 'slacks' in params_data:
+        planning_parameters.slacks.read_slacks_parameters(params_data['slacks'])
     planning_parameters.print_results_to_file = bool(params_data['print_results_to_file'])
+    planning_parameters.solver_params.read_solver_parameters(params_data['solver'])
     planning_parameters.solver_params.read_solver_parameters(params_data['solver'])
