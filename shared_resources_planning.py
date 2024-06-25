717225158ad6f107e5bb1b31aad312a994e2c62e
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-06-25 15:10:54 +0100

    Previous iteration. update_transmission_coordination_model_and_solve() updated.

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -1130,12 +1130,22 @@ def update_transmission_coordination_model_and_solve(transmission_network, model
 
                 # Update VOLTAGE and POWER FLOW variables at connection point
                 for p in model[year][day].periods:
-                    model[year][day].dual_v_sqr_req[dn, p].fix(dual_vsqr[node_id][year][day][p] / s_base)
-                    model[year][day].dual_pf_p_req[dn, p].fix(dual_pf[node_id][year][day]['p'][p] / s_base)
-                    model[year][day].dual_pf_q_req[dn, p].fix(dual_pf[node_id][year][day]['q'][p] / s_base)
+
+                    model[year][day].dual_v_sqr_req[dn, p].fix(dual_vsqr['current'][node_id][year][day][p] / s_base)
                     model[year][day].v_sqr_req[dn, p].fix(vsqr_req['dso']['current'][node_id][year][day][p])
+                    if params.previous_iter['v']:
+                        model[year][day].dual_v_sqr_prev[dn, p].fix(dual_vsqr['prev'][node_id][year][day][p] / s_base)
+                        model[year][day].v_sqr_prev[dn, p].fix(vsqr_req['tso']['prev'][node_id][year][day][p])
+
+                    model[year][day].dual_pf_p_req[dn, p].fix(dual_pf['current'][node_id][year][day]['p'][p] / s_base)
+                    model[year][day].dual_pf_q_req[dn, p].fix(dual_pf['current'][node_id][year][day]['q'][p] / s_base)
                     model[year][day].p_pf_req[dn, p].fix(pf_req['dso']['current'][node_id][year][day]['p'][p] / s_base)
                     model[year][day].q_pf_req[dn, p].fix(pf_req['dso']['current'][node_id][year][day]['q'][p] / s_base)
+                    if params.previous_iter['pf']:
+                        model[year][day].dual_pf_p_prev[dn, p].fix(dual_pf['prev'][node_id][year][day]['p'][p] / s_base)
+                        model[year][day].dual_pf_q_prev[dn, p].fix(dual_pf['prev'][node_id][year][day]['q'][p] / s_base)
+                        model[year][day].p_pf_prev[dn, p].fix(pf_req['tso']['prev'][node_id][year][day]['p'][p] / s_base)
+                        model[year][day].q_pf_prev[dn, p].fix(pf_req['tso']['prev'][node_id][year][day]['q'][p] / s_base)
 
                 # Update shared ESS capacity and power requests
                 shared_ess_idx = transmission_network.network[year][day].get_shared_energy_storage_idx(node_id)
