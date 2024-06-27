9e1d165de8b831d5e51ca128dedbad604487be31
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-06-27 10:45:59 +0100

    Network model. Penalty for flexibility usage added (congestion management)

--- network.py
+++ network.py
@@ -2101,6 +2101,14 @@ def _compute_objective_function_value(network, model, params):
                             qc_curt = pe.value(model.qc_curt_down[c, s_m, s_o, p] + model.qc_curt_up[c, s_m, s_o, p])
                             obj_scenario += PENALTY_LOAD_CURTAILMENT * network.baseMVA * (pc_curt + qc_curt)
 
+                # Demand side flexibility
+                if params.fl_reg:
+                    for c in model.loads:
+                        for p in model.periods:
+                            flex_p_up = model.flex_p_up[c, s_m, s_o, p]
+                            flex_p_down = model.flex_p_down[c, s_m, s_o, p]
+                            obj_scenario += PENALTY_FLEXIBILITY_USAGE * network.baseMVA * (flex_p_down - flex_p_up)
+
                 obj += obj_scenario * (network.prob_market_scenarios[s_m] * network.prob_operation_scenarios[s_o])
 
     return obj
