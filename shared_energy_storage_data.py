5cb71e4efafc930bcc01ea90c5f150c361695c07
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-07-02 09:02:48 +0100

    Shared ESS. Optimization parameters. Correction

--- shared_energy_storage_data.py
+++ shared_energy_storage_data.py
@@ -525,8 +525,8 @@ def _optimize(model, params, from_warm_start=False):
         solver.options['output_file'] = 'optim_log.txt'
 
     if params.solver == 'ipopt':
-        solver.options['tol'] = params.solver_params.solver_tol
-        solver.options['linear_solver'] = params.solver_params.linear_solver
+        solver.options['tol'] = params.solver_tol
+        solver.options['linear_solver'] = params.linear_solver
         solver.options['mu_strategy'] = 'adaptive'
 
     result = solver.solve(model, tee=params.verbose)
