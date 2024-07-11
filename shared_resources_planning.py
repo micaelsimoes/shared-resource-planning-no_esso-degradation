787d904662a8e69ace5e7f9abfa30bc82241126b
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-07-11 15:56:50 +0100

    Corrections.

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -830,31 +830,22 @@ def update_transmission_model_to_admm(transmission_network, model, consensus_var
             if transmission_network.params.obj_type == OBJ_MIN_COST:
                 init_of_value = abs(pe.value(model[year][day].objective))
             if isclose(init_of_value, 0.00, abs_tol=SMALL_TOLERANCE):
-                init_of_value = 1.00
+                init_of_value = SMALL_TOLERANCE
             obj = copy(model[year][day].objective.expr) / init_of_value
 
             for dn in model[year][day].active_distribution_networks:
+
                 adn_node_id = transmission_network.active_distribution_network_nodes[dn]
-
-                p_norm = max([abs(value) for value in consensus_vars['interface']['pf']['dso']['current'][adn_node_id][year][day]['p']]) / s_base
-                if isclose(p_norm, 0.00, abs_tol=SMALL_TOLERANCE):
-                    p_norm = 1.00
-
-                q_norm = max([abs(value) for value in consensus_vars['interface']['pf']['dso']['current'][adn_node_id][year][day]['q']]) / s_base
-                if isclose(q_norm, 0.00, abs_tol=SMALL_TOLERANCE):
-                    q_norm = 1.00
 
                 for p in model[year][day].periods:
 
-                    '''
-                    init_p = abs(consensus_vars['interface']['pf']['dso']['current'][adn_node_id][year][day]['p'][p]) / s_base
-                    if isclose(init_p, 0.00, abs_tol=SMALL_TOLERANCE):
-                        init_p = 1.00
+                    p_norm = abs(consensus_vars['interface']['pf']['dso']['current'][adn_node_id][year][day]['p'][p]) / s_base
+                    if isclose(p_norm, 0.00, abs_tol=SMALL_TOLERANCE):
+                        p_norm = SMALL_TOLERANCE
 
-                    init_q = abs(consensus_vars['interface']['pf']['dso']['current'][adn_node_id][year][day]['q'][p]) / s_base
-                    if isclose(init_q, 0.00, abs_tol=SMALL_TOLERANCE):
-                        init_q = 1.00
-                    '''
+                    q_norm = abs(consensus_vars['interface']['pf']['dso']['current'][adn_node_id][year][day]['q'][p]) / s_base
+                    if isclose(q_norm, 0.00, abs_tol=SMALL_TOLERANCE):
+                        q_norm = SMALL_TOLERANCE
 
                     constraint_v_req = (model[year][day].expected_interface_vmag_sqr[dn, p] - model[year][day].v_sqr_req[dn, p])
                     obj += model[year][day].dual_v_sqr_req[dn, p] * constraint_v_req
@@ -871,7 +862,7 @@ def update_transmission_model_to_admm(transmission_network, model, consensus_var
 
                 shared_ess_rating = abs(transmission_network.network[year][day].shared_energy_storages[e].s)
                 if isclose(shared_ess_rating, 0.00, abs_tol=SMALL_TOLERANCE):
-                    shared_ess_rating = 1.00
+                    shared_ess_rating = SMALL_TOLERANCE
 
                 for p in model[year][day].periods:
                     constraint_ess_p = (model[year][day].expected_shared_ess_p[e, p] - model[year][day].p_ess_req[e, p]) / (2 * shared_ess_rating)
@@ -909,9 +900,7 @@ def update_distribution_models_to_admm(distribution_networks, models, consensus_
                             dso_model[year][day].e[ref_node_idx, s_m, s_o, p].fixed = False
                             dso_model[year][day].e[ref_node_idx, s_m, s_o, p].setub(v_max + SMALL_TOLERANCE)
                             dso_model[year][day].e[ref_node_idx, s_m, s_o, p].setlb(v_min - SMALL_TOLERANCE)
-                            dso_model[year][day].f[ref_node_idx, s_m, s_o, p].fixed = False
-                            dso_model[year][day].f[ref_node_idx, s_m, s_o, p].setub(SMALL_TOLERANCE)
-                            dso_model[year][day].f[ref_node_idx, s_m, s_o, p].setlb(-SMALL_TOLERANCE)
+                            dso_model[year][day].f[ref_node_idx, s_m, s_o, p].fix(0.00)
                             dso_model[year][day].pg[ref_gen_idx, s_m, s_o, p].fixed = False
                             dso_model[year][day].pg[ref_gen_idx, s_m, s_o, p].setub(None)
                             dso_model[year][day].pg[ref_gen_idx, s_m, s_o, p].setlb(None)
@@ -944,24 +933,24 @@ def update_distribution_models_to_admm(distribution_networks, models, consensus_
                 if distribution_network.params.obj_type == OBJ_MIN_COST:
                     init_of_value = abs(pe.value(dso_model[year][day].objective))
                 if isclose(init_of_value, 0.00, abs_tol=SMALL_TOLERANCE):
-                    init_of_value = 1.00
+                    init_of_value = SMALL_TOLERANCE
                 obj = copy(dso_model[year][day].objective.expr) / init_of_value
 
                 shared_ess_idx = distribution_network.network[year][day].get_shared_energy_storage_idx(ref_node_id)
                 shared_ess_rating = abs(distribution_network.network[year][day].shared_energy_storages[shared_ess_idx].s)
                 if isclose(shared_ess_rating, 0.00, abs_tol=SMALL_TOLERANCE):
-                    shared_ess_rating = 1.00
+                    shared_ess_rating = SMALL_TOLERANCE
 
                 # Augmented Lagrangian -- Interface power flow (residual balancing)
-                p_norm = max([abs(value) for value in consensus_vars['interface']['pf']['dso']['current'][node_id][year][day]['p']])
-                if isclose(p_norm, 0.00, abs_tol=SMALL_TOLERANCE):
-                    p_norm = 1.00
-
-                q_norm = max([abs(value) for value in consensus_vars['interface']['pf']['dso']['current'][node_id][year][day]['q']])
-                if isclose(q_norm, 0.00, abs_tol=SMALL_TOLERANCE):
-                    q_norm = 1.00
-
                 for p in dso_model[year][day].periods:
+
+                    p_norm = abs(consensus_vars['interface']['pf']['dso']['current'][node_id][year][day]['p'][p]) / s_base
+                    if isclose(p_norm, 0.00, abs_tol=SMALL_TOLERANCE):
+                        p_norm = SMALL_TOLERANCE
+
+                    q_norm = abs(consensus_vars['interface']['pf']['dso']['current'][node_id][year][day]['q'][p]) / s_base
+                    if isclose(q_norm, 0.00, abs_tol=SMALL_TOLERANCE):
+                        q_norm = SMALL_TOLERANCE
 
                     # Voltage magnitude
                     constraint_vmag_req = (dso_model[year][day].expected_interface_vmag_sqr[p] - dso_model[year][day].v_sqr_req[p])
