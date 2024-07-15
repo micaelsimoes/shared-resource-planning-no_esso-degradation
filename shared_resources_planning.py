758d58a762681a1278e41dc1e7fb127d95d1690b
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-07-15 10:02:06 +0100

    Update. Initialization

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -546,7 +546,7 @@ def create_distribution_networks_models(distribution_networks, consensus_vars, c
                             obj += PENALTY_EXPECTED_PF_DEVIATION * s_base * (dso_model[year][day].qg[ref_gen_idx, s_m, s_o, p] - dso_model[year][day].expected_interface_pf_q[p]) ** 2
                             obj += PENALTY_EXPECTED_SESS_DEVIATION * s_base * (dso_model[year][day].shared_es_pnet[shared_ess_idx, s_m, s_o, p] - dso_model[year][day].expected_shared_ess_p[p]) ** 2
                             obj += PENALTY_EXPECTED_SESS_DEVIATION * s_base * (dso_model[year][day].shared_es_qnet[shared_ess_idx, s_m, s_o, p] - dso_model[year][day].expected_shared_ess_q[p]) ** 2
-                    dso_model[year][day].interface_cons.add((dso_model[year][day].expected_interface_vmag_sqr[p] ** 2) == expected_vmag_sqr)
+                    dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_vmag_sqr[p] == expected_vmag_sqr)
                 dso_model[year][day].objective.expr = obj
 
         # Run SMOPF
