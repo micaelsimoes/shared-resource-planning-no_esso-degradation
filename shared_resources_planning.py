09a9eb7158f93480d4adc8849f5af3972ed07956
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-07-15 10:36:16 +0100

    Update. Initialization

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -545,8 +545,8 @@ def create_distribution_networks_models(distribution_networks, consensus_vars, c
                         for s_o in dso_model[year][day].scenarios_operation:
                             omega_oper = distribution_network.network[year][day].prob_operation_scenarios[s_o]
                             obj += PENALTY_EXPECTED_VMAG_DEVIATION * ((dso_model[year][day].e[ref_node_idx, s_m, s_o, p] ** 2) - dso_model[year][day].expected_interface_vmag_sqr[p]) ** 2
-                            #obj += PENALTY_EXPECTED_PF_DEVIATION * s_base * (dso_model[year][day].pg[ref_gen_idx, s_m, s_o, p] - dso_model[year][day].expected_interface_pf_p[p]) ** 2
-                            #obj += PENALTY_EXPECTED_PF_DEVIATION * s_base * (dso_model[year][day].qg[ref_gen_idx, s_m, s_o, p] - dso_model[year][day].expected_interface_pf_q[p]) ** 2
+                            obj += PENALTY_EXPECTED_PF_DEVIATION * s_base * (dso_model[year][day].pg[ref_gen_idx, s_m, s_o, p] - dso_model[year][day].expected_interface_pf_p[p]) ** 2
+                            obj += PENALTY_EXPECTED_PF_DEVIATION * s_base * (dso_model[year][day].qg[ref_gen_idx, s_m, s_o, p] - dso_model[year][day].expected_interface_pf_q[p]) ** 2
                             #obj += PENALTY_EXPECTED_SESS_DEVIATION * s_base * (dso_model[year][day].shared_es_pnet[shared_ess_idx, s_m, s_o, p] - dso_model[year][day].expected_shared_ess_p[p]) ** 2
                             #obj += PENALTY_EXPECTED_SESS_DEVIATION * s_base * (dso_model[year][day].shared_es_qnet[shared_ess_idx, s_m, s_o, p] - dso_model[year][day].expected_shared_ess_q[p]) ** 2
                             #expected_vmag_sqr += omega_market * omega_oper * (dso_model[year][day].e[ref_node_idx, s_m, s_o, p] ** 2)
@@ -557,18 +557,18 @@ def create_distribution_networks_models(distribution_networks, consensus_vars, c
                     if distribution_network.params.relax_equalities:
                         #dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_vmag_sqr[p] <= expected_vmag_sqr + EQUALITY_TOLERANCE)
                         #dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_vmag_sqr[p] >= expected_vmag_sqr - EQUALITY_TOLERANCE)
-                        dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_pf_p[p] <= expected_pf_p + EQUALITY_TOLERANCE)
-                        dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_pf_p[p] >= expected_pf_p - EQUALITY_TOLERANCE)
-                        dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_pf_q[p] <= expected_pf_q + EQUALITY_TOLERANCE)
-                        dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_pf_q[p] >= expected_pf_q - EQUALITY_TOLERANCE)
+                        #dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_pf_p[p] <= expected_pf_p + EQUALITY_TOLERANCE)
+                        #dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_pf_p[p] >= expected_pf_p - EQUALITY_TOLERANCE)
+                        #dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_pf_q[p] <= expected_pf_q + EQUALITY_TOLERANCE)
+                        #dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_pf_q[p] >= expected_pf_q - EQUALITY_TOLERANCE)
                         dso_model[year][day].interface_cons.add(dso_model[year][day].expected_shared_ess_p[p] <= expected_ess_p + EQUALITY_TOLERANCE)
                         dso_model[year][day].interface_cons.add(dso_model[year][day].expected_shared_ess_p[p] >= expected_ess_p - EQUALITY_TOLERANCE)
                         dso_model[year][day].interface_cons.add(dso_model[year][day].expected_shared_ess_q[p] <= expected_ess_q + EQUALITY_TOLERANCE)
                         dso_model[year][day].interface_cons.add(dso_model[year][day].expected_shared_ess_q[p] >= expected_ess_q - EQUALITY_TOLERANCE)
                     else:
                         #dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_vmag_sqr[p] == expected_vmag_sqr)
-                        dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_pf_p[p] == expected_pf_p)
-                        dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_pf_q[p] == expected_pf_q)
+                        #dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_pf_p[p] == expected_pf_p)
+                        #dso_model[year][day].interface_cons.add(dso_model[year][day].expected_interface_pf_q[p] == expected_pf_q)
                         dso_model[year][day].interface_cons.add(dso_model[year][day].expected_shared_ess_p[p] == expected_ess_p)
                         dso_model[year][day].interface_cons.add(dso_model[year][day].expected_shared_ess_q[p] == expected_ess_q)
                 dso_model[year][day].objective.expr = obj
