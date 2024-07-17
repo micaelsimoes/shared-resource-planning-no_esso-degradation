c3f7568892bd7fe81369db0640961d3f6aeca5ce
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-07-17 11:11:15 +0100

    Update.

--- main.py
+++ main.py
@@ -73,11 +73,8 @@ def shared_resources_planning(working_directory, specification_filename):
     results = transmission_network.optimize(tn_model)
     processed_results = transmission_network.process_results(tn_model, results)
     transmission_network.write_optimization_results_to_excel(processed_results)
-    #sensitivities = transmission_network.get_sensitivities(tn_model)
-    #print(sensitivities)
 
-    '''
-    #candidate_solution = planning_problem.get_initial_candidate_solution()
+    candidate_solution = planning_problem.get_initial_candidate_solution()
     distribution_networks = planning_problem.distribution_networks
     for node_id in distribution_networks:
         distribution_network = distribution_networks[node_id]
@@ -86,7 +83,6 @@ def shared_resources_planning(working_directory, specification_filename):
         results = distribution_network.optimize(dn_model)
         processed_results = distribution_network.process_results(dn_model, results)
         distribution_network.write_optimization_results_to_excel(processed_results)
-    '''
 
     '''
     candidate_solution = planning_problem.get_initial_candidate_solution()
