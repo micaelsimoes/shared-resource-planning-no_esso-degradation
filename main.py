3b1e9ecdc7760f50fa0c39a886d32665a8b80dd1
Micael Filipe Simões <micael.f.simoes@inesctec.pt>
2024-08-22 15:28:26 +0100

    Update.

--- main.py
+++ main.py
@@ -62,47 +62,41 @@ def shared_resources_planning(working_directory, specification_filename):
     planning_problem.plot_diagram()
 
     candidate_solution = planning_problem.get_initial_candidate_solution()
-    #planning_problem.run_operational_planning(candidate_solution=candidate_solution, print_results=True, debug_flag=False)
+    # planning_problem.run_operational_planning(candidate_solution=candidate_solution, print_results=True, debug_flag=False)
     #planning_problem.run_without_coordination(print_results=True)
     #planning_problem.run_planning_problem()
 
-    '''
-    transmission_network = planning_problem.transmission_network
-    candidate_solution = planning_problem.get_initial_candidate_solution()
-    tn_model = transmission_network.build_model()
-    transmission_network.update_model_with_candidate_solution(tn_model, candidate_solution['total_capacity'])
-    results = transmission_network.optimize(tn_model)
-    processed_results = transmission_network.process_results(tn_model, results)
-    transmission_network.write_optimization_results_to_excel(processed_results)
-    '''
+    # transmission_network = planning_problem.transmission_network
+    # candidate_solution = planning_problem.get_initial_candidate_solution()
+    # tn_model = transmission_network.build_model()
+    # transmission_network.update_model_with_candidate_solution(tn_model, candidate_solution['total_capacity'])
+    # results = transmission_network.optimize(tn_model)
+    # processed_results = transmission_network.process_results(tn_model, results)
+    # transmission_network.write_optimization_results_to_excel(processed_results)
 
-    '''
-    candidate_solution = planning_problem.get_initial_candidate_solution()
-    distribution_networks = planning_problem.distribution_networks
-    for node_id in distribution_networks:
-        distribution_network = distribution_networks[node_id]
-        dn_model = distribution_network.build_model()
-        distribution_network.update_model_with_candidate_solution(dn_model, candidate_solution['total_capacity'])
-        results = distribution_network.optimize(dn_model)
-        processed_results = distribution_network.process_results(dn_model, results)
-        distribution_network.write_optimization_results_to_excel(processed_results)
-    '''
+    # candidate_solution = planning_problem.get_initial_candidate_solution()
+    # distribution_networks = planning_problem.distribution_networks
+    # for node_id in distribution_networks:
+    #     distribution_network = distribution_networks[node_id]
+    #     dn_model = distribution_network.build_model()
+    #     distribution_network.update_model_with_candidate_solution(dn_model, candidate_solution['total_capacity'])
+    #     results = distribution_network.optimize(dn_model)
+    #     processed_results = distribution_network.process_results(dn_model, results)
+    #     distribution_network.write_optimization_results_to_excel(processed_results)
 
-    '''
-    candidate_solution = planning_problem.get_initial_candidate_solution()
-    planning_problem.shared_ess_data.update_data_with_candidate_solution(candidate_solution['total_capacity'])
-    esso_model = planning_problem.shared_ess_data.build_subproblem()
-    planning_problem.shared_ess_data.update_model_with_candidate_solution(esso_model, candidate_solution['investment'])
-    results = planning_problem.shared_ess_data.optimize(esso_model)
-    planning_problem.shared_ess_data.write_optimization_results_to_excel(esso_model)
-    '''
+    # candidate_solution = planning_problem.get_initial_candidate_solution()
+    # planning_problem.shared_ess_data.update_data_with_candidate_solution(candidate_solution['total_capacity'])
+    # esso_model = planning_problem.shared_ess_data.build_subproblem()
+    # planning_problem.shared_ess_data.update_model_with_candidate_solution(esso_model, candidate_solution['investment'])
+    # results = planning_problem.shared_ess_data.optimize(esso_model)
+    # planning_problem.shared_ess_data.write_optimization_results_to_excel(esso_model)
 
     candidate_solution = planning_problem.get_initial_candidate_solution()
     planning_problem.shared_ess_data.update_data_with_candidate_solution(candidate_solution['total_capacity'])
     esso_model = planning_problem.shared_ess_data.build_subproblem_decomposed()
     planning_problem.shared_ess_data.update_model_with_candidate_solution_decomposed(esso_model, candidate_solution['investment'])
-    results = planning_problem.shared_ess_data.optimize(esso_model)
-
+    results = planning_problem.shared_ess_data.optimize_decomposed(esso_model)
+    planning_problem.shared_ess_data.write_optimization_results_to_excel_decomposed(esso_model)
 
     print('==========================================================================================================')
     print('                                                 END                                                      ')
