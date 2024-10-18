1364e2d2b810497eac3cb6106430f419d1d0f9b6
micaelsimoes <micaelsimoes@gmail.com>
2024-10-18 14:04:59 +0100

    Update.

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -1463,8 +1463,48 @@ def _run_operational_planning_without_coordination(planning_problem):
         results['dso'][node_id] = dict()
 
         # Build model, fix candidate solution, and Run S-MPOPF model
+        distribution_network.update_data_with_candidate_solution(candidate_solution)
         dso_model = distribution_network.build_model()
         distribution_network.update_model_with_candidate_solution(dso_model, candidate_solution)
+
+        # Update model with expected interface values
+        for year in distribution_network.years:
+            for day in distribution_network.days:
+
+                ref_node_id = distribution_network.network[year][day].get_reference_node_id()
+                ref_node_idx = distribution_network.network[year][day].get_node_idx(ref_node_id)
+                ref_gen_idx = distribution_network.network[year][day].get_reference_gen_idx()
+                shared_ess_idx = distribution_network.network[year][day].get_shared_energy_storage_idx(ref_node_id)
+
+                # Add interface expected variables
+                dso_model[year][day].expected_interface_vmag_sqr = pe.Var(dso_model[year][day].periods, domain=pe.NonNegativeReals, initialize=1.00)
+                dso_model[year][day].expected_interface_pf_p = pe.Var(dso_model[year][day].periods, domain=pe.Reals, initialize=0.00)
+                dso_model[year][day].expected_interface_pf_q = pe.Var(dso_model[year][day].periods, domain=pe.Reals, initialize=0.00)
+                dso_model[year][day].expected_shared_ess_p = pe.Var(dso_model[year][day].periods, domain=pe.Reals, initialize=0.00)
+                dso_model[year][day].expected_shared_ess_q = pe.Var(dso_model[year][day].periods, domain=pe.Reals, initialize=0.00)
+
+                dso_model[year][day].interface_expected_values = pe.ConstraintList()
+                for p in dso_model[year][day].periods:
+                    expected_vmag_sqr = 0.00
+                    expected_pf_p = 0.00
+                    expected_pf_q = 0.00
+                    expected_ess_p = 0.00
+                    expected_ess_q = 0.00
+                    for s_m in dso_model[year][day].scenarios_market:
+                        omega_market = distribution_network.network[year][day].prob_market_scenarios[s_m]
+                        for s_o in dso_model[year][day].scenarios_operation:
+                            omega_oper = distribution_network.network[year][day].prob_operation_scenarios[s_o]
+                            expected_vmag_sqr += omega_market * omega_oper * dso_model[year][day].e[ref_node_idx, s_m, s_o, p] ** 2
+                            expected_pf_p += omega_market * omega_oper * dso_model[year][day].pg[ref_gen_idx, s_m, s_o, p]
+                            expected_pf_q += omega_market * omega_oper * dso_model[year][day].qg[ref_gen_idx, s_m, s_o, p]
+                            expected_ess_p += omega_market * omega_oper * dso_model[year][day].shared_es_pnet[shared_ess_idx, s_m, s_o, p]
+                            expected_ess_q += omega_market * omega_oper * dso_model[year][day].shared_es_qnet[shared_ess_idx, s_m, s_o, p]
+                    dso_model[year][day].interface_expected_values.add(dso_model[year][day].expected_interface_vmag_sqr[p] == expected_vmag_sqr)
+                    dso_model[year][day].interface_expected_values.add(dso_model[year][day].expected_interface_pf_p[p] == expected_pf_p)
+                    dso_model[year][day].interface_expected_values.add(dso_model[year][day].expected_interface_pf_q[p] == expected_pf_q)
+                    dso_model[year][day].interface_expected_values.add(dso_model[year][day].expected_shared_ess_p[p] == expected_ess_p)
+                    dso_model[year][day].interface_expected_values.add(dso_model[year][day].expected_shared_ess_q[p] == expected_ess_q)
+
         results['dso'][node_id] = distribution_network.optimize(dso_model)
 
         # Get initial interface PF values
