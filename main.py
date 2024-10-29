e345d720450324b40a63e261198ccb3c970afec1
micaelsimoes <micaelsimoes@gmail.com>
2024-10-29 17:40:52 +0000

    Consensus criteria updated.

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def shared_resources_planning(working_directory, specification_filename):
     #         transmission_network.network[year][day].shared_energy_storages = list()
     transmission_network.update_data_with_candidate_solution(candidate_solution)
     tn_model = transmission_network.build_model()
-    # transmission_network.update_model_with_candidate_solution(tn_model, candidate_solution['total_capacity'])
+    transmission_network.update_model_with_candidate_solution(tn_model, candidate_solution['total_capacity'])
     results = transmission_network.optimize(tn_model)
     processed_results = transmission_network.process_results(tn_model, results)
     transmission_network.write_optimization_results_to_excel(processed_results)
