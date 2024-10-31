167820d86ef4c13af8e46815603cd172607d0b76
micaelsimoes <micaelsimoes@gmail.com>
2024-10-31 17:55:26 +0000

    Debug. Num scenarios.

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -843,7 +843,7 @@ def update_transmission_model_to_admm(transmission_network, model, consensus_var
             # Update costs (penalties) for the coordination procedure
             model[year][day].penalty_ess_usage.fix(0.00)
             if transmission_network.params.obj_type == OBJ_MIN_COST:
-                model[year][day].cost_res_curtailment.fix(0.00)
+                model[year][day].cost_res_curtailment.fix(COST_GENERATION_CURTAILMENT)
                 model[year][day].cost_load_curtailment.fix(COST_CONSUMPTION_CURTAILMENT)
             elif transmission_network.params.obj_type == OBJ_CONGESTION_MANAGEMENT:
                 model[year][day].penalty_gen_curtailment.fix(0.00)
