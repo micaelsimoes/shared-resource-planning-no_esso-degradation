799bdd0b2d7385c8d1ecc66e3871a3f714b5773e
micaelsimoes <micaelsimoes@gmail.com>
2024-11-04 16:43:05 +0000

    Debug. Voltage removed from consensus.

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -505,7 +505,7 @@ def create_transmission_network_model(transmission_network, consensus_vars, cand
                     interface_pf_q = consensus_vars['interface']['pf']['dso']['current'][adn_node_id][year][day]['q'][p] / s_base
                     shared_ess_p = consensus_vars['ess']['dso']['current'][adn_node_id][year][day]['p'][p] / s_base
                     shared_ess_q = consensus_vars['ess']['dso']['current'][adn_node_id][year][day]['q'][p] / s_base
-                    tso_model[year][day].expected_interface_vmag_sqr[dn, p].fix(interface_v)
+                    # tso_model[year][day].expected_interface_vmag_sqr[dn, p].fix(interface_v)
                     tso_model[year][day].expected_interface_pf_p[dn, p].fix(interface_pf_p)
                     tso_model[year][day].expected_interface_pf_q[dn, p].fix(interface_pf_q)
                     tso_model[year][day].expected_shared_ess_p[shared_ess_idx, p].fix(shared_ess_p)
