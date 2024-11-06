7442483af45a58529eaadd8800fe79a9e5b23b99
micaelsimoes <micaelsimoes@gmail.com>
2024-11-06 14:10:30 +0000

    ADMM parameters updated with previous iteration field.

--- admm_parameters.py
+++ admm_parameters.py
@@ -8,8 +8,8 @@ class ADMMParameters:
                     'stationarity': {'v': 1e-2, 'pf': 1e-3, 'ess': 1e-3}}
         self.num_max_iters = 1000
         self.adaptive_penalty = False
-        self.rho = {'v': dict(), 'pf': dict(), 'ess': dict()}
         self.previous_iter = {'v': False, 'pf': False, 'ess': False}
+        self.rho = {'v': dict(), 'pf': dict(), 'ess': dict()}
 
     def read_parameters_from_file(self, params_data):
         _read_parameters_from_file(self, params_data)
@@ -24,9 +24,9 @@ def _read_parameters_from_file(admm_params, params_data):
     admm_params.tol['stationarity']['ess'] = float(params_data['tol']['stationarity']['ess'])
     admm_params.num_max_iters = int(params_data['num_max_iters'])
     admm_params.adaptive_penalty = bool(params_data['adaptive_penalty'])
-    admm_params.rho['v'] = float(params_data['rho']['v'])
-    admm_params.rho['pf'] = float(params_data['rho']['pf'])
-    admm_params.rho['ess'] = float(params_data['rho']['ess'])
     admm_params.previous_iter['v'] = bool(params_data['previous_iteration']['v'])
     admm_params.previous_iter['pf'] = bool(params_data['previous_iteration']['pf'])
     admm_params.previous_iter['ess'] = bool(params_data['previous_iteration']['ess'])
+    admm_params.rho['v'] = params_data['rho']['v']
+    admm_params.rho['pf'] = params_data['rho']['pf']
+    admm_params.rho['ess'] = params_data['rho']['ess']
