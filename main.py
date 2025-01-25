844c74907ffc0885cd2617003934474e9aac5bd3
micaelsimoes <micaelsimoes@gmail.com>
2025-01-25 12:11:48 +0000

    CS7. High Investment. Test

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def shared_resources_planning(working_directory, specification_filename):
     # planning_problem.plot_diagram()
 
     candidate_solution = planning_problem.get_initial_candidate_solution()
-    # planning_problem.run_without_coordination(print_results=True)
+    planning_problem.run_without_coordination(print_results=True)
     # planning_problem.run_operational_planning(candidate_solution=candidate_solution, print_results=True, debug_flag=False)
     # planning_problem.run_planning_problem()
 
@@ -87,10 +87,6 @@ def shared_resources_planning(working_directory, specification_filename):
         #         distribution_network.network[year][day].shared_energy_storages = list()
         distribution_network.update_data_with_candidate_solution(candidate_solution['total_capacity'])
         dn_model = distribution_network.build_model()
-        for year in distribution_network.years:
-            for day in distribution_network.days:
-                dn_model[year][day].penalty_flex_usage.fix(PENALTY_FLEXIBILITY_USAGE)
-                dn_model[year][day].penalty_ess_usage.fix(PENALTY_ESS_USAGE)
         distribution_network.update_model_with_candidate_solution(dn_model, candidate_solution['total_capacity'])
         results = distribution_network.optimize(dn_model)
         processed_results = distribution_network.process_results(dn_model, results)
