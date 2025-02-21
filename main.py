2f4e02d5f1d10cb42b17540998f744319e44d763
micaelsimoes <micaelsimoes@gmail.com>
2025-02-21 13:54:34 +0000

    Debug.

--- main.py
+++ main.py
@@ -81,16 +81,11 @@ def shared_resources_planning(working_directory, specification_filename):
     distribution_networks = planning_problem.distribution_networks
     for node_id in distribution_networks:
         distribution_network = distribution_networks[node_id]
-        # for year in distribution_network.years:
-        #     for day in distribution_network.days:
-        #         distribution_network.network[year][day].shared_energy_storages = list()
-        distribution_network.update_data_with_candidate_solution(candidate_solution['total_capacity'])
-        dn_model = distribution_network.build_model()
         for year in distribution_network.years:
             for day in distribution_network.days:
-                dn_model[year][day].penalty_flex_usage.fix(0.00)
-                dn_model[year][day].penalty_ess_usage.fix(0.00)
-        distribution_network.update_model_with_candidate_solution(dn_model, candidate_solution['total_capacity'])
+                distribution_network.network[year][day].shared_energy_storages = list()
+        # distribution_network.update_data_with_candidate_solution(candidate_solution['total_capacity'])
+        dn_model = distribution_network.build_model()
         results = distribution_network.optimize(dn_model)
         processed_results = distribution_network.process_results(dn_model, results)
         distribution_network.write_optimization_results_to_excel(processed_results)
