07908e00e8df68115f70360b64208a9dcce6d964
micaelsimoes <micaelsimoes@gmail.com>
2025-05-29 14:59:38 +0100

    Model. Construction. Node balance constraints.

--- network_data.py
+++ network_data.py
@@ -34,10 +34,7 @@ class NetworkData:
         for year in self.years:
             network_models[year] = dict()
             for day in self.days:
-                if self.network[year][day].is_transmission:
-                    network_models[year][day] = self.network[year][day].build_model(self.params)
-                else:
-                    network_models[year][day] = self.network[year][day].build_model_v2(self.params)
+                network_models[year][day] = self.network[year][day].build_model_v2(self.params)
         return network_models
 
     def optimize(self, model, from_warm_start=False):
