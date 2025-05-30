fdf406577132e6b721b5425e8f98708a578f8499
micaelsimoes <micaelsimoes@gmail.com>
2025-05-30 11:34:29 +0100

    Model. Soft constraint option. Rollback

--- model_construction_helpers.py
+++ model_construction_helpers.py
@@ -282,10 +282,8 @@ def sg_sqr_rule(m, g, s_m, s_o, p, network, params):
     generator = network.generators[g]
     if not generator.is_curtaillable():
         return pe.Constraint.Skip
-    if params.use_soft_constraints:
-        return pe.inequality(-EQUALITY_TOLERANCE, m.sg_sqr[g, s_m, s_o, p] - (m.pg[g, s_m, s_o, p]**2 + m.qg[g, s_m, s_o, p]**2), EQUALITY_TOLERANCE)
-    else:
-        return m.sg_sqr[g, s_m, s_o, p] == m.pg[g, s_m, s_o, p] ** 2 + m.qg[g, s_m, s_o, p] ** 2
+    return pe.inequality(-EQUALITY_TOLERANCE, m.sg_sqr[g, s_m, s_o, p] - (m.pg[g, s_m, s_o, p]**2 + m.qg[g, s_m, s_o, p]**2), EQUALITY_TOLERANCE)
+    #return m.sg_sqr[g, s_m, s_o, p] == m.pg[g, s_m, s_o, p] ** 2 + m.qg[g, s_m, s_o, p] ** 2
 
 # sg_abs² ≈ sg_sqr
 def sg_abs_rule(m, g, s_m, s_o, p, network):
