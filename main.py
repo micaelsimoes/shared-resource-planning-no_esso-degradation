90096b592427e460d97f1e40ebb89fd10581e6d4
micaelsimoes <micaelsimoes@gmail.com>
2025-05-31 14:43:20 +0100

    ADMM models. Update to consider vmag_sqr, pc_node, qc_node.

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def shared_resources_planning(working_directory, specification_filename):
     # for year in transmission_network.years:
     #     for day in transmission_network.days:
     #         transmission_network.network[year][day].shared_energy_storages = list()
-    transmission_network.update_data_with_candidate_solution(candidate_solution)
+    transmission_network.update_data_with_candidate_solution(candidate_solution['total_capacity'])
     tn_model = transmission_network.build_model()
     transmission_network.update_model_with_candidate_solution(tn_model, candidate_solution['total_capacity'])
     results = transmission_network.optimize(tn_model)
