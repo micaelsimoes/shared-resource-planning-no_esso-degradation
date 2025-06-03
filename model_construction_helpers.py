2da6bde9e0276004cf31fd329314a699ed5a253a
micaelsimoes <micaelsimoes@gmail.com>
2025-06-03 16:07:13 +0100

    Network model. Shared ESS. Different models introduced.

--- model_construction_helpers.py
+++ model_construction_helpers.py
@@ -146,8 +146,8 @@ def slack_flow_bounds(m, b, s_m, s_o, p, network, params):
     branch = network.branches[b]
     if not branch.status:
         return (0.0, EQUALITY_TOLERANCE)
-    rating_sqr = (branch.rate / network.baseMVA) ** 2
-    return (0.0, SIJ_VIOLATION_ALLOWED * rating_sqr)
+    rating = (branch.rate / network.baseMVA)
+    return (0.0, (SIJ_VIOLATION_ALLOWED * rating) ** 2)
 
 
 # Consumption, Pc
