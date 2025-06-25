91030710cc91402f5efafe5a1f6455d1239aac3f
micaelsimoes <micaelsimoes@gmail.com>
2025-06-25 16:54:03 +0100

    Test. optimize() method parallelized.

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -1198,8 +1198,6 @@ def update_distribution_coordination_models_and_solve_sequential(distribution_ne
         model = models[node_id]
         distribution_network = distribution_networks[node_id]
 
-        print(f'[INFO] \t\t\t - Updating active distribution network connected to node {node_id}...')
-
         for year in distribution_network.years:
             for day in distribution_network.days:
 
