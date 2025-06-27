834a816d31db4f5b3e5a7713f5a23453e5de8a80
micaelsimoes <micaelsimoes@gmail.com>
2025-06-27 11:30:00 +0100

    Warm start, tests.

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def shared_resources_planning(working_directory, specification_filename):
         results = distribution_network.optimize(dn_model)
         processed_results = distribution_network.process_results(dn_model, results)
         distribution_network.write_optimization_results_to_excel(processed_results)
-    planning_problem.run_without_coordination(print_results=True)
+    # planning_problem.run_without_coordination(print_results=True)
 
 
     # candidate_solution = planning_problem.get_initial_candidate_solution()
