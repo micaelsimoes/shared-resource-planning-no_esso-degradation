178d989f3e8268953c254f2bcecd1992d58f2bb7
micaelsimoes <micaelsimoes@gmail.com>
2025-06-27 09:25:36 +0100

    Model equalities. Update.

--- model_construction_helpers.py
+++ model_construction_helpers.py
@@ -409,7 +409,7 @@ def ess_sch_def(m, e, s_m, s_o, p):
 
 
 def ess_sdch_def(m, e, s_m, s_o, p):
-    return m.es_sdch[e, s_m, s_o, p]**2 - (m.es_pdch[e, s_m, s_o, p]**2 + m.es_qdch[e, s_m, s_o, p]**2)
+    return m.es_sdch[e, s_m, s_o, p]**2 == (m.es_pdch[e, s_m, s_o, p]**2 + m.es_qdch[e, s_m, s_o, p]**2)
 
 
 def ess_phi_ch_limits_lower(m, e, s_m, s_o, p, network):
