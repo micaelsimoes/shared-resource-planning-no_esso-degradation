9d6fbb1f4a4b6d242f26ce228ffc8ee94aebf43f
micaelsimoes <micaelsimoes@gmail.com>
2025-06-28 14:11:28 +0100

    Model. Equality constraints. Rollback.

--- network.py
+++ network.py
@@ -1816,6 +1816,7 @@ def _run_smopf(network, model, params, from_warm_start=False):
             solver.options['mu_strategy'] = 'adaptive'
         else:
             solver.options['nlp_scaling_method'] = 'gradient-based'
+            solver.options['mu_strategy'] = 'adaptive'
 
         # solver.options['bound_relax_factor'] = 1e-6
         # solver.options['honor_original_bounds'] = 'no'
