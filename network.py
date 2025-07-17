f7909e04284fb682cc46365be74b25818d76b166
micaelsimoes <micaelsimoes@gmail.com>
2025-07-17 11:49:03 +0100

    Planning. Test.

--- network.py
+++ network.py
@@ -1812,11 +1812,19 @@ def _run_smopf(network, model, params, from_warm_start=False):
         solver.options['tol'] = params.solver_params.solver_tol
         solver.options['linear_solver'] = params.solver_params.linear_solver
         solver.options['nlp_scaling_method'] = 'gradient-based'
-        solver.options['bound_relax_factor'] = EQUALITY_TOLERANCE
-        if network.is_transmission:
-            solver.options['mu_strategy'] = 'adaptive'
         if params.solver_params.limited_memory:
             solver.options['hessian_approximation'] = 'limited-memory'
+        if network.is_transmission:
+            solver.options['mu_strategy'] = 'adaptive'
+        else:
+            solver.options['bound_relax_factor'] = EQUALITY_TOLERANCE
+            solver.options['honor_original_bounds'] = 'no'
+            # solver.options['mu_strategy'] = 'adaptive'
+
+        # solver.options['bound_relax_factor'] = 1e-6
+        # solver.options['honor_original_bounds'] = 'no'
+        # solver.options['acceptable_iter'] = 10
+        # solver.options['acceptable_tol'] = 1e-3
 
     try:
         result = solver.solve(model, tee=params.solver_params.verbose)
