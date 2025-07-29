cc2e5e7465ccddfe225724a0a18e933286890daa
micaelsimoes <micaelsimoes@gmail.com>
2025-07-29 14:13:45 +0100

    sg_sqr removed.

--- model_construction_helpers.py
+++ model_construction_helpers.py
@@ -340,6 +340,9 @@ def sg_abs_rule(m, g, s_m, s_o, p, network, params):
 
 
 def sg_curt_rule(m, g, s_m, s_o, p, network, params):
+    generator = network.generators[g]
+    if not generator.is_curtaillable() or not generator.status[p]:
+        return pe.Constraint.Skip
     return m.sg_curt[g, s_m, s_o, p] == m.sg_init[g, s_m, s_o, p] - m.sg_abs[g, s_m, s_o, p] ** 2
 
 
