22ec571b02a8a04d21bdbd6de86cac77d3f97dc2
micaelsimoes <micaelsimoes@gmail.com>
2025-07-29 14:33:15 +0100

    sg_sqr removed.

--- model_construction_helpers.py
+++ model_construction_helpers.py
@@ -334,7 +334,7 @@ def sg_curt_rule(m, g, s_m, s_o, p, network, params):
     generator = network.generators[g]
     if not generator.is_curtaillable() or not generator.status[p]:
         return pe.Constraint.Skip
-    return m.sg_curt[g, s_m, s_o, p] == m.sg_init[g, s_m, s_o, p] - m.sg_abs[g, s_m, s_o, p] ** 2
+    return m.sg_curt[g, s_m, s_o, p] == m.sg_init[g, s_m, s_o, p] - m.sg_abs[g, s_m, s_o, p]
 
 
 def power_factor_rule_upper(m, g, s_m, s_o, p, network):
