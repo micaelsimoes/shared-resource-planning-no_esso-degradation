9f37468ce604c9871bc1fa776c104afecadec131
micaelsimoes <micaelsimoes@gmail.com>
2025-08-21 15:33:16 +0100

    Test.

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -1536,8 +1536,7 @@ def check_consensus_convergence(planning_problem, consensus_vars, params, debug_
                 interface_v_base = planning_problem.transmission_network.network[year][day].get_node_base_kv(node_id)
                 interface_transf_rating = planning_problem.distribution_networks[node_id].network[year][day].get_interface_branch_rating()
                 shared_ess_rating = abs(planning_problem.transmission_network.network[year][day].shared_energy_storages[shared_ess_idx].s) * s_base
-                if isclose(shared_ess_rating, 0.00, abs_tol=0.50):
-                    shared_ess_rating = 1.00
+                shared_ess_rating = max(abs(planning_problem.transmission_network.network[year][day].shared_energy_storages[shared_ess_idx].s) * s_base, 0.10)
 
                 for p in range(planning_problem.num_instants):
                     sum_rel_abs_error_vmag += abs(consensus_vars['vmag']['tso']['current'][node_id][year][day][p] - consensus_vars['vmag']['dso']['current'][node_id][year][day][p]) / interface_v_base
@@ -1592,9 +1591,7 @@ def check_stationary_convergence(planning_problem, consensus_vars, params):
 
                 interface_v_base = planning_problem.transmission_network.network[year][day].get_node_base_kv(node_id)
                 interface_transf_rating = planning_problem.distribution_networks[node_id].network[year][day].get_interface_branch_rating()
-                shared_ess_rating = abs(planning_problem.transmission_network.network[year][day].shared_energy_storages[shared_ess_idx].s) * s_base
-                if isclose(shared_ess_rating, 0.00, abs_tol=0.50):
-                    shared_ess_rating = 1.00
+                shared_ess_rating = max(abs(planning_problem.transmission_network.network[year][day].shared_energy_storages[shared_ess_idx].s) * s_base, 0.10)
 
                 for p in range(planning_problem.num_instants):
 
