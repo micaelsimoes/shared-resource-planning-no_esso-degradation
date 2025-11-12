b908d05ab96ef4c6fba0e7ba981f3fb813cdaf67
micaelsimoes <micaelsimoes@gmail.com>
2025-11-12 18:11:38 +0000

    Case study. Update.

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def shared_resources_planning(working_directory, specification_filename):
     # planning_problem.run_operational_planning(type='hierarchical', num_steps=4, print_results=True, debug_flag=False, filename=f'{planning_problem.name}_operational_planning_results_hierarchical_N={4}')
     # planning_problem.run_operational_planning(type='hierarchical', num_steps=8, print_results=True, debug_flag=False, filename=f'{planning_problem.name}_operational_planning_results_hierarchical_N={8}')
     planning_problem.run_operational_planning(type='distributed', print_results=True, debug_flag=False, filename=f'{planning_problem.name}_operational_planning_results_distributed_without ESS')
-    planning_problem.run_operational_planning(type='distributed', candidate_solution=candidate_solution, print_results=True, debug_flag=False, filename=f'{planning_problem.name}_operational_planning_results_distributed_with ESS')
+    # planning_problem.run_operational_planning(type='distributed', candidate_solution=candidate_solution, print_results=True, debug_flag=False, filename=f'{planning_problem.name}_operational_planning_results_distributed_with ESS')
 
     # planning_problem.run_operational_planning(candidate_solution=candidate_solution, print_results=True, debug_flag=False)
     # planning_problem.run_planning_problem()
@@ -95,6 +95,7 @@ def shared_resources_planning(working_directory, specification_filename):
     #     results = distribution_network.optimize(dn_model)
     #     processed_results = distribution_network.process_results(dn_model, results)
     #     distribution_network.write_optimization_results_to_excel(processed_results)
+    #     break
 
     # candidate_solution = planning_problem.get_initial_candidate_solution()
     # planning_problem.shared_ess_data.update_data_with_candidate_solution(candidate_solution['total_capacity'])
