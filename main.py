9965dd4d78d41a32ca70fb9ab04ea47970338166
micaelsimoes <micaelsimoes@gmail.com>
2025-12-01 10:04:19 +0000

    Case studies. Update.

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def shared_resources_planning(working_directory, specification_filename):
     candidate_solution = planning_problem.get_test_candidate_solution(s_inv=10.00, e_inv=20.00)
     planning_problem.run_operational_planning(type='uncoordinated', print_results=True, debug_flag=False)
     # planning_problem.run_operational_planning(type='centralized', print_results=True, debug_flag=False)
-    planning_problem.run_operational_planning(type='hierarchical', num_steps=1, print_results=True, debug_flag=False, filename=f'{planning_problem.name}_operational_planning_results_hierarchical_N={1}')
+    # planning_problem.run_operational_planning(type='hierarchical', num_steps=1, print_results=True, debug_flag=False, filename=f'{planning_problem.name}_operational_planning_results_hierarchical_N={1}')
     # planning_problem.run_operational_planning(type='hierarchical', num_steps=2, print_results=True, debug_flag=False, filename=f'{planning_problem.name}_operational_planning_results_hierarchical_N={2}')
     # planning_problem.run_operational_planning(type='hierarchical', num_steps=4, print_results=True, debug_flag=False, filename=f'{planning_problem.name}_operational_planning_results_hierarchical_N={4}')
     # planning_problem.run_operational_planning(type='hierarchical', num_steps=8, print_results=True, debug_flag=False, filename=f'{planning_problem.name}_operational_planning_results_hierarchical_N={8}')
@@ -79,13 +79,6 @@ def shared_resources_planning(working_directory, specification_filename):
     # for year in transmission_network.years:
     #     for day in transmission_network.days:
     #         transmission_network.network[year][day].shared_energy_storages = list()
-    # transmission_network.params.fl_reg = True
-    # tn_model = transmission_network.build_model()
-    # results = transmission_network.optimize(tn_model)
-    # processed_results = transmission_network.process_results(tn_model, results)
-    # transmission_network.write_optimization_results_to_excel(processed_results)
-    #
-    # transmission_network.params.fl_reg = False
     # tn_model = transmission_network.build_model()
     # results = transmission_network.optimize(tn_model)
     # processed_results = transmission_network.process_results(tn_model, results)
