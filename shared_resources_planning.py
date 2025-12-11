47a37ad9237ae2e4fcc4dd78daf4e7a89f32296a
micaelsimoes <micaelsimoes@gmail.com>
2025-12-11 15:51:50 +0000

    Case studies. Update

--- shared_resources_planning.py
+++ shared_resources_planning.py
@@ -102,14 +102,14 @@ class SharedResourcesPlanning:
 
         print('[INFO] Running PLANNING PROBLEM WITHOUT COORDINATION...')
 
-        # print('[INFO] \t - Transmission Network. Power factor control switched off!')
-        # print('[INFO] \t - Transmission Network. Flexible load control switched off!')
-        # self.transmission_network.params.fl_reg = False
-        # for year in self.transmission_network.years:
-        #     for day in self.transmission_network.days:
-        #         for generator in self.transmission_network.network[year][day].generators:
-        #             if generator.is_curtaillable():
-        #                 generator.power_factor_control = False
+        print('[INFO] \t - Transmission Network. Power factor control switched off!')
+        print('[INFO] \t - Transmission Network. Flexible load control switched off!')
+        self.transmission_network.params.fl_reg = False
+        for year in self.transmission_network.years:
+            for day in self.transmission_network.days:
+                for generator in self.transmission_network.network[year][day].generators:
+                    if generator.is_curtaillable():
+                        generator.power_factor_control = False
 
         print('[INFO] \t - Distribution Networks. Power factor control switched off!')
         print('[INFO] \t - Distribution Networks. Flexible load control switched off!')
@@ -125,14 +125,14 @@ class SharedResourcesPlanning:
         if print_results:
             self.write_operational_planning_results_without_coordination_to_excel(models, results, execution_time=execution_time)
 
-        # print('[INFO] \t - Transmission Network. Power factor control switched back on!')
-        # print('[INFO] \t - Transmission Network. Flexible load control switched back on!')
-        # self.transmission_network.params.fl_reg = True
-        # for year in self.transmission_network.years:
-        #     for day in self.transmission_network.days:
-        #         for generator in self.transmission_network.network[year][day].generators:
-        #             if generator.is_curtaillable():
-        #                 generator.power_factor_control = True
+        print('[INFO] \t - Transmission Network. Power factor control switched back on!')
+        print('[INFO] \t - Transmission Network. Flexible load control switched back on!')
+        self.transmission_network.params.fl_reg = True
+        for year in self.transmission_network.years:
+            for day in self.transmission_network.days:
+                for generator in self.transmission_network.network[year][day].generators:
+                    if generator.is_curtaillable():
+                        generator.power_factor_control = True
 
         print('[INFO] \t - Distribution Networks. Power factor control switched back on!')
         print('[INFO] \t - Distribution Networks. Flexible load control switched back on!')
